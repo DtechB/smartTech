db2299c837a02ccc2a2efe4ff545de4c911cdbaf
dtechb <d.bayati28@gmail.com>
2021-12-08 06:22:28 +0330

    add login and register and forgot-password page

--- mainPage/urls.py
+++ mainPage/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='main-page-index')
+    path('', views.index, name='main-page-index'),
+    path('login/', views.login, name='main-page-login'),
+    path('register/', views.register, name='main-page-register'),
+    path('login/forgot-password/', views.forgot, name='main-page-forgot-pass')
 ]
--- mainPage/views.py
+++ mainPage/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'mainPage/index.html')
+
+
+def login(request):
+    return render(request, 'mainPage/login.html')
+
+
+def register(request):
+    return render(request, 'mainPage/register.html')
+
+
+def forgot(request):
+    return render(request, 'mainPage/forgot-password.html')
