4ea5ba12630258dd7ea8352f03b066a727835180
dtechb <d.bayati28@gmail.com>
2021-12-16 19:41:10 +0330

    add html file user account and set css and js files

--- account/views.py
+++ account/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from mainPage.models import User, SmartPhone
 
 
 @login_required
 def home(request):
-    return render(request, 'account/panel.html')
+    queryset = SmartPhone.objects.filter(user_id=1)
+    return render(request, 'account/panel.html', context={'phone': queryset})
