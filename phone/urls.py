2a8ef5852f6ae9fb73dc8fbe53099d1b1db4dd85
dtechb <d.bayati28@gmail.com>
2021-12-29 10:04:21 +0330

    add button in phone content for add phone to comparison with ajax.

--- phone/urls.py
+++ phone/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 app_name = 'phone'
 urlpatterns = [
+    path('api/<int:pk>', views.link, name='api_link'),
+    path('api/<int:pk>/addphone', views.add_comparison_phone, name='api_add_comparison'),
     path('', views.index, name='phones'),
     path('phone/<slug:phone>/<int:pk>/', views.single_phone, name='single_phone')
 ]
--- phone/views.py
+++ phone/views.py
@@ -1,27 +1,35 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.http import JsonResponse
 from mainPage.models import SmartPhone
 from .forms import AddComparisonForm
 
 
 def index(request):
     phones = SmartPhone.objects.all()
-    if request.method == 'POST':
-        form = AddComparisonForm(request.POST)
-        if form.is_valid():
-            phone_id = request.POST.get('id')
-            phone = SmartPhone.objects.get(smartphone_id=phone_id)
-            user = request.user
-            if user.smartphone_set.count() <= 2:
-                phone.user.add(user)
-    else:
-        form = AddComparisonForm()
-
+    count_phone = request.user.smartphone_set.count()
     return render(request, 'phone/phone-content.html', context={
         'phones': phones,
-        'form': form
+        'count': count_phone
     })
 
 
 def single_phone(request, phone, pk):
     phone = get_object_or_404(SmartPhone, slug=phone, pk=pk)
     return render(request, 'phone/phone-single.html', {'phones': phone})
+
+
+def link(request, pk):
+    user = request.user
+    linke = get_object_or_404(SmartPhone, pk=pk)
+    return JsonResponse({
+        'phones': user.smartphone_set.count(),
+        'user': user.is_authenticated
+    })
+
+
+def add_comparison_phone(request, pk):
+    user = request.user
+    linke = get_object_or_404(SmartPhone, pk=pk)
+    if user.smartphone_set.count() <= 2:
+        linke.user.add(user)
+    return redirect('phone:phones')
