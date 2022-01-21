f700f917fe5dddf3159c62fc99e4ac42cba3fa86
dtechb <d.bayati28@gmail.com>
2022-01-21 19:12:04 +0330

    edit sum features in user panel.

--- account/views.py
+++ account/views.py
@@ -19,11 +19,13 @@ from django.core.mail import EmailMessage
 def home(request):
     queryset1 = SmartPhone.objects.filter(user=request.user)
     queryset2 = Post.objects.filter(userpost=request.user)
-    queryset3 = Post.objects.order_by('-updated').all()[:6]
+    ran = range(3 - queryset1.count())
+    queryset3 = Post.objects.order_by('-updated').all()[:7]
     return render(request, 'account/panel.html', context={
         'phone': queryset1,
         'posts': queryset2,
-        'all_posts': queryset3
+        'all_posts': queryset3,
+        'range': ran
     })
 
 
