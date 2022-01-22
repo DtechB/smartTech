ff2143ab75429525b9094b8aa6fe5fca5c089648
dtechb <d.bayati28@gmail.com>
2022-01-22 23:19:50 +0330

    edit some features in main page.

--- mainPage/views.py
+++ mainPage/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import SmartPhone, Post, User
-from .forms import LoginForms, RegisterForms, ForgotForms
+from django.core.mail import EmailMessage
 
 
 def index(request):
@@ -9,6 +9,21 @@ def index(request):
     phones_number = SmartPhone.objects.all().count()
     posts_number = Post.objects.all().count()
     users_number = User.objects.all().count()
+
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        mail_subject = request.POST.get('subject')
+        message = f'Hi {name} dear,\n\n' \
+                  + request.POST.get('message') \
+                  + '\n\n we check your question and You will be answered as soon as ' \
+                    'possible after the review.\nThanks, SmartTech.'
+        to_email = request.POST.get('email')
+        email = EmailMessage(
+            mail_subject, message, to=[to_email]
+        )
+        email.send()
+        return redirect('account:contact_sent')
+
     return render(request, 'mainPage/index.html', context={
         'phones': phones,
         'posts': posts,
