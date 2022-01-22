0c285fa40baacbc284ecbe81c16ca938fb6aba2b
dtechb <d.bayati28@gmail.com>
2022-01-22 20:38:06 +0330

    edit some section in main page and add recent posts.

--- mainPage/views.py
+++ mainPage/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render
-from .models import SmartPhone, Post
+from .models import SmartPhone, Post, User
 from .forms import LoginForms, RegisterForms, ForgotForms
 
 
 def index(request):
     phones = SmartPhone.objects.all()[:3]
-    return render(request, 'mainPage/index.html', context={'phones': phones})
+    posts = Post.objects.all()[:3]
+    phones_number = SmartPhone.objects.all().count()
+    posts_number = Post.objects.all().count()
+    users_number = User.objects.all().count()
+    return render(request, 'mainPage/index.html', context={
+        'phones': phones,
+        'posts': posts,
+        'user_count': users_number,
+        'post_count': posts_number,
+        'phone_count': phones_number,
+    })
 
 
 def not_found_404(request, exception):
